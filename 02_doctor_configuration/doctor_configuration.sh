0e9019081048324646dbbdbaa1e9f1a00617cfbf
Joel-hou <yanhuaqingshang@163.com>
2017-09-27 15:03:05 +0800

    nothing

--- 02_doctor_configuration/doctor_configuration.sh
+++ 02_doctor_configuration/doctor_configuration.sh
@@ -10,45 +10,22 @@ ansible controller -m script -a "./notification_configuration_controller.sh"
 # create doctor datasource
 openstack congress datasource create doctor doctor
 
-# function defination
-function _congress_add_rule {
-    name=$1
-    policy=$2
-    rule=$3
+# add policy
+openstack congress policy rule create \
+    --name host_down classification \
+    'host_down(host) :-
+        doctor:events(hostname=host, type="compute.host.down", status="down")'
 
-    if ! openstack congress policy rule list $policy | grep -q -e "// Name: $name$" ; then
-        openstack congress policy rule create --name $name $policy "$rule"
-    fi
-}
+openstack congress policy rule create \
+    --name active_instance_in_host classification \
+    'active_instance_in_host(vmid, host) :-
+        nova:servers(id=vmid, host_name=host, status="ACTIVE")'
 
-function _congress_del_rule {
-    name=$1
-    policy=$2
+openstack congress policy rule create \
+    --name pause_vm_states classification \
+    'execute[nova:servers.pause(vmid)] :-
+        host_down(host),
+        active_instance_in_host(vmid, host)'
 
-    if openstack congress policy rule list $policy | grep -q -e "^// Name: $name$" ; then
-        openstack congress policy rule delete $policy $name
-    fi
-}
-
-function _congress_add_rules {
-    _congress_add_rule host_down classification \
-        'host_down(host) :-
-            doctor:events(hostname=host, type="compute.host.down", status="down")'
-
-    _congress_add_rule active_instance_in_host classification \
-        'active_instance_in_host(vmid, host) :-
-            nova:servers(id=vmid, host_name=host, status="ACTIVE")'
-
-    #_congress_add_rule host_force_down classification \
-        'execute[nova:services.force_down(host, "nova-compute", "True")] :-
-            host_down(host)'
-
-    _congress_add_rule pause_vm_states classification \
-        'execute[nova:servers.pause(vmid)] :-
-            host_down(host),
-            active_instance_in_host(vmid, host)'
-}
-
-_congress_add_rules
 # create alarm
 aodh alarm create --name test_alarm --type event --alarm-action "http://127.0.0.1:12346/" --repeat-actions false --event-type compute.instance.update --query "traits.state=string::pause"
