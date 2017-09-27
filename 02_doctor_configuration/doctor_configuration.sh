78a0b768c95a59f81efaf4d5e3045e293605fd5c
Joel-hou <yanhuaqingshang@163.com>
2017-09-27 14:42:43 +0800

    nothing

--- 02_doctor_configuration/doctor_configuration.sh
+++ 02_doctor_configuration/doctor_configuration.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
 # on stack VM overcloud, please make sure your environment variable is clean
-# source ~/overcloudrc
-# !before any other operation
+set -e
 
 # configuration for all controller nodes
 ansible controller -m script -a "./notification_configuration_controller.sh"
 
+source ~/overcloudrc
 # create doctor datasource
 openstack congress datasource create doctor doctor
 
 # create congress policy
-openstack congress policy rule create \
-    --name host_down classification \
-    'host_down(host) :-
-        doctor:events(hostname=host, type="compute.host.down", status="down")'
-
 openstack congress policy rule create \
     --name active_instance_in_host classification \
     'active_instance_in_host(vmid, host) :-
@@ -27,6 +22,12 @@ openstack congress policy rule create \
     'execute[nova:servers.pause(vmid)] :-
         host_down(host),
         active_instance_in_host(vmid, host)'
+
+openstack congress policy rule create \
+    --name host_down classification \
+    'host_down(host) :-
+        doctor:events(hostname=host, type="compute.host.down", status="down")'
+
 
 # create alarm
 aodh alarm create --name test_alarm --type event --alarm-action "http://127.0.0.1:12346/" --repeat-actions false --event-type compute.instance.update --query "traits.state=string::pause"
