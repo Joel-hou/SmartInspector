197588543376cae31a16811dc0a22fe712192171
Joel-hou <yanhuaqingshang@163.com>
2017-10-13 16:58:38 +0800

    add  copy bootstrap cloudify manager and deploy clearwater to home directory

--- 04_cloudify&clearwater_deployment/bootstrap_cloudify_and_deploy_clearwater.sh
+++ 04_cloudify&clearwater_deployment/bootstrap_cloudify_and_deploy_clearwater.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # on test_zhian
+# you had better copy bootstrap_cloudify_manager.sh, deploy_clearwater_via_cloudify_CLI.sh
+# and bootstrap_cloudify_and_deploy_clearwater.sh to your home directory for a clean pythonenv
+# environment
+
 # deploy cloudify and clearwater
 source ./bootstrap_cloudify_manager.sh
 source ./deploy_clearwater_via_cloudify_CLI.sh
