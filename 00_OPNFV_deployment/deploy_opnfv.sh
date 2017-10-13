40d8813905588c35ed38028cf0bc2208cb67164d
Joel-hou <yanhuaqingshang@163.com>
2017-10-13 11:00:35 +0800

    add NAT virbr0 to br-external

--- 00_OPNFV_deployment/deploy_opnfv.sh
+++ 00_OPNFV_deployment/deploy_opnfv.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-# on HW server
+# on jumphost 
+# HW: 4 core/E7-8890-v3 512G RAM
 # to deploy opnfv via Apex
 
-# HW: 4 core/E7-8890-v3 512G RAM
+# NAT  virbr0 to br-external, so undercloud can have access to Internet 
+sysctl net.ipv4.ip_forward=1
+iptables -t nat -A POSTROUTING -s 192.168.122.0/24 -j SNAT --to 192.168.32.20
+iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
+
 opnfv-clean
 cd /etc/opnfv-apex 
-screen -S apex_deploy_screen \
-	opnfv-deploy -v --virtual-cpus 8 \
-    --virtual-default-ram 64 --virtual-compute-ram 96 \
-    -n network_settings.yaml -d os-nosdn-nofeature-ha.yaml --debug > apex.log
+opnfv-deploy -v --virtual-cpus 16 --virtual-default-ram 64 --virtual-compute-ram 96 --virtual-computes 4 -n network_settings.yaml -d os-nosdn-nofeature-noha.yaml --debug > apex.log
