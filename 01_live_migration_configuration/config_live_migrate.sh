837c5c4adef4280674353f5b196f9a8da0fd5ff2
Joel-hou <yanhuaqingshang@163.com>
2017-10-13 11:39:34 +0800

    add ./

--- 01_live_migration_configuration/config_live_migrate.sh
+++ 01_live_migration_configuration/config_live_migrate.sh
@@ -3,7 +3,7 @@
 source ~/stackrc
 controller0ip=$(nova list | grep overcloud-controller-0 | cut -d'|' -f 7 | cut -d'=' -f 2)
 # install nfs-server on controller-0
-ansible controller-0 -m script -a "config_live_migrate_controller.sh" --sudo
+ansible controller-0 -m script -a "./config_live_migrate_controller.sh" --sudo
 
 # install nfs-client on all compute node 
 ansible compute -m script -a "./config_live_migrate_compute.sh $controller0ip" --sudo
