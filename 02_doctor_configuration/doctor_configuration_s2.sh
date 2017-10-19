ee87509aa9c30547ca79b6470fecf40316584c56
Joel-hou <yanhuaqingshang@163.com>
2017-10-19 17:31:43 +0800

    remove useless sentences

--- 02_doctor_configuration/doctor_configuration_s2.sh
+++ 02_doctor_configuration/doctor_configuration_s2.sh
@@ -4,16 +4,16 @@
 set -e
 source ~/overcloudrc
 
-# configuration for all controller nodes
-ansible controller -m script -a "./notification_configuration_controller.sh" --sudo
+# notification configuration is not necessary for our s2(solution 2)
+# let congress do automatic fault recovery job
 
 # create doctor datasource
 openstack congress datasource create doctor doctor
 
 # add policy
-# xuan xue:Syntax error for rule::Literal doctor:events(hostname=host, status="down", type="compute.host.down") uses named arguments, but the schema is unknown.
-# the following line should work
-openstack congress policy rule create     --name host_down classification     'host_down(host) :-
+openstack congress policy rule create \
+    --name host_down classification \
+        'host_down(host) :-
         doctor:events(hostname=host, type="compute.host.down", status="down")'
 
 openstack congress policy rule create \
@@ -21,12 +21,9 @@ openstack congress policy rule create \
     'active_instance_in_host(vmid, host) :-
         nova:servers(id=vmid, host_name=host, status="ACTIVE")'
 
-# make compute 3 node as our fault recovery default compute node
+# make compute 3 node as our fault recovery default node
 openstack congress policy rule create \
     --name live_migrate_vm classification \
     'execute[nova:servers.live_migrate(vmid,"overcloud-novacompute-3.opnfvlf.org","False","False")] :-
         host_down(host),
         active_instance_in_host(vmid, host)'
-
-# create alarm not necessary for our solution 2,our zabbix script will automatically migrate VM
-# aodh only responsible for notifying application_manager 
